bb668d5305b882968897e51ac6c22a47fd1953ea
Mrigank Shekhar <mrigank.shekhar012@gmail.com>
2021-07-07 15:47:09 +0530

    dashboard leads

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './components/App/App.css';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Preferences from './components/Preferences/Preferences';
+import useToken from './components/App/useToken';
+
+import Container from 'react-bootstrap/Container';
+
+function setToken(userToken) {
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+  const tokenString = sessionStorage.getItem('token');
+  const userToken = JSON.parse(tokenString);
+  return userToken?.access;
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = '/';
+}
+
+function App() {
+  const { token, setToken } = useToken();
+
+  // function logout() {
+  //   localStorage.clear();
+  //   window.location.href = '/';
+  // }
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <Container fluid>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="">Affiliate Home</a>
+      <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#" onClick={logout}>Sign out</a>
+        </li>
+      </ul>
+    </header>
+    <div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="">
+              <span data-feather="home"></span>
+              Leads
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="">
+              <span data-feather="home"></span>
+              Profile
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+       <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+    </main>
+  </div>
+</div>
+    
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,162 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+const ai = axios.create({
+  baseURL: 'https://api.thetradingprofits.com/api/leads/'
+});
+
+const tokenString = localStorage.getItem('token');
+const userToken = JSON.parse(tokenString);
+const token = userToken?.access;
+
+class Dashboard extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+    	data : [],
+	    name: '',
+	  	email: "",
+	    landing_page: '',
+	    subject: '',
+	    message: 'Test {{link}}',
+	    uuid: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  showPlan = () => {
+    ai.get('', { 
+		headers: {
+			"Authorization": `Bearer ${token}`,
+			"Content-Type": "application/json"
+		} 
+	})
+    .then((res) => {
+      this.setState({data: res.data})
+    })
+  }
+
+  landingPageData = () => {
+    axios.get("https://api.thetradingprofits.com/api/pages/", {
+		headers: {
+			"Authorization": `Bearer ${token}`
+		} 
+	})
+    .then((res) => {
+		this.setState({uuid: res.data})
+    })
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let d = 
+    {
+		"name": this.state.name,
+		"email": this.state.email,
+		"landing_page": this.state.landing_page,
+		"subject": this.state.subject,
+		"message": this.state.message + token
+    }
+
+    ai.post('', d, {
+		headers: {
+			"Authorization": "Bearer " + token,
+			"Content-Type": "application/json"
+		} 
+	})
+    .then((res) => {
+      console.log(res.data)
+    })
+
+  }
+
+  componentDidMount(){
+    this.showPlan();
+    this.landingPageData();
+  }
+
+  render() {
+    return (
+		<>
+	      	<div>
+	            <div>
+	            <h4>Leads</h4>
+	                <table class="table table-striped table-sm">
+	                    <thead>
+	                        <tr>
+	                            <th>S.NO</th>
+	                            <th>Name</th>
+	                            <th>Email</th>
+	                            <th>Landing Page</th>
+	                            <th>Subject</th>
+	                            <th>Message</th>
+	                        </tr>
+	                    </thead>
+	                    <tbody>
+	                    	{
+	                    		this.state.data.map((d, i) => 
+	                    			(
+		                    			<tr key={i} id={i+1}>
+		                    				<td>{d.id}</td>
+		                    				<td>{d.name}</td>
+		                    				<td>{d.email}</td>
+		                    				<td>{d.landing_page}</td>
+		                    				<td>No subject</td>
+		                    				<td>No message</td>
+		                    			</tr>
+	                    			)
+	                    		)
+	                    	}
+	                    </tbody>
+	                </table>
+	            </div>
+	            <div style = {{ marginTop : 50 }}>
+	            	<h4>New Lead</h4>
+		        	<form onSubmit={this.handleSubmit}>
+		        		<label>
+				          Name:
+				          <input name="name" type="text" value={this.state.name} onChange={this.handleChange} required class="form-control"/>
+				        </label><br />
+				        <label>
+				          Email:
+				          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required class="form-control"/>
+				        </label><br />
+				        <label>
+				          Landing Page:
+				          <select name="landing_page" value={this.state.landing_page} onChange={this.handleChange} required class="form-control">
+				          	<option value="" disabled>Select...</option>
+
+				          	{
+	                    		this.state.uuid.map((d, i) => 
+	                    			(
+		                    			<option key={i} value={d.uuid}>{d.uuid}</option>
+	                    			)
+	                    		)
+	                    	}
+				          </select>
+				        </label><br />
+				        <label>
+				          Subject:
+				          <input type="text" name="subject" value={this.state.subject} onChange={this.handleChange} required class="form-control"/>
+				        </label><br />
+				        <label>
+				          Message:
+				          <input type="text" name="message" value={this.state.message} onChange={this.handleChange} required class="form-control"/>
+				        </label><br /><br />
+						<input type="submit" value="Submit" />
+				    </form>
+	            </div>
+	        </div>
+	    </>
+    )
+  }
+}
+
+export default Dashboard;
